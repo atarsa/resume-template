58bc8a255d2e0370d12eb29d1def784a0b3801a7
ATarsa <agnieszkatarsa@gmail.com>
2020-06-16 19:37:27 +0100

    Add Education section

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,12 +1,30 @@
 import React from "react"
-
+import YAMLData from "../content/education.yaml"
 import StyledSection from "./styles/StyledSection"
+import { StyledH3, StyledH4 } from "./styles/StyledHeadings"
+import FlexContainer from "./styles/FlexContainer"
 import Heading from "./Heading"
+
+const School = ({ school }) => (
+  <>
+    <FlexContainer>
+      <StyledH3>{school.name}</StyledH3>
+      <p className="orange">{school.timeline}</p>
+    </FlexContainer>
+
+    <StyledH4>{school.degree}</StyledH4>
+
+    <p>{school.description}</p>
+  </>
+)
 
 const Education = () => {
   return (
     <StyledSection id="education">
       <Heading title="Education" />
+      {YAMLData.content.map((data, index) => {
+        return <School school={data.school} key={`school-${index}`} />
+      })}
     </StyledSection>
   )
 }
