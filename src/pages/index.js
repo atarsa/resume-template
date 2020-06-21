b8f3d21ff9354bb03375dd1533cc0e71f7a052e0
ATarsa <agnieszkatarsa@gmail.com>
2020-06-21 12:16:12 +0100

    Add useEffect hook to Intersection Observer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Layout from "../layout/Layout"
 import About from "../components/About"
 import Experience from "../components/Experience"
@@ -8,34 +8,35 @@ import Interests from "../components/Interests"
 import Awards from "../components/Awards"
 
 const IndexPage = () => {
-  // Set up Intersection Observer to implement active navigation:
-  // get sections to observe
-  const sections = document.querySelectorAll("section")
+  useEffect(() => {
+    // Set up Intersection Observer to implement active navigation:
+    // get sections to observe
+    const sections = document.querySelectorAll("section")
 
-  // get all nav links elements
-  const navLinks = document.querySelectorAll("#sidebar a")
+    // get all nav links elements
+    const navLinks = document.querySelectorAll("#sidebar a")
 
-  let observer = new IntersectionObserver(
-    entries => {
-      entries.forEach(entry => {
-        console.log("entry id", entry.target.id)
-        if (entry.intersectionRatio > 0) {
-          // check on which section we are in
-          // add class .active to nav link with that target name
-          // otherwise remove .active class from that element
-          navLinks.forEach(link => {
-            return link.innerText.toLowerCase() === entry.target.id
-              ? link.classList.add("active")
-              : link.classList.remove("active")
-          })
-        }
-      })
-    },
-    { threshold: 0.5 }
-  )
+    let observer = new IntersectionObserver(
+      entries => {
+        entries.forEach(entry => {
+          if (entry.intersectionRatio > 0) {
+            // check on which section we are in
+            // add class .active to nav link with that target name
+            // otherwise remove .active class from that element
+            navLinks.forEach(link => {
+              return link.innerText.toLowerCase() === entry.target.id
+                ? link.classList.add("active")
+                : link.classList.remove("active")
+            })
+          }
+        })
+      },
+      { threshold: 0.8 }
+    )
 
-  sections.forEach(section => {
-    observer.observe(section)
+    sections.forEach(section => {
+      observer.observe(section)
+    })
   })
 
   return (
